4ba4589c4ab18899f1e0c7699bc015ba77fdd1ec
serhat-kose <serhatkse94@gmail.com>
2021-04-03 14:54:54 +0300

    v-for using

--- myapp/src/App.vue
+++ myapp/src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <Demo msg="Welcome to Your Vue.js App"/>
+    
+    <Demo msg="Hello Vue.js"/>
+    
   </div>
 </template>
 
@@ -9,7 +10,7 @@
 import Demo from './components/Demo.vue'
 
 export default {
-  name: 'App',
+  name: 'app',
   components: {
     Demo
   }
@@ -17,12 +18,5 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- myapp/src/components/Demo.vue
+++ myapp/src/components/Demo.vue
@@ -1,19 +1,30 @@
 <template>
-<div>
-  <h1>HELLO VUE</h1>
+<div class="holder">
+ <ul>
+   <li v-for="(data,index) in lessons" :key='index'>
+     {{index}}.{{data.lesson}}
+   </li>
+ </ul>
+
 </div>
 </template>
 
 <script>
 export default {
   name: 'Demo',
-  props: {
-    msg: String
-  }
+ data(){
+   return{
+     lessons:[
+       {"lesson":"Math"},
+       {"lesson":"Eng"}
+     ]
+   }
+ }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
 
 </style>
